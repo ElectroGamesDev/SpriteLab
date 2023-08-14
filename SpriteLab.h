a856f88ebacd59a35ea8878e7a156cbacb2ac74a
Electro Games <electrogames@cubicpe.net>
2023-08-14 01:31:54 -0500

    Update SpriteLab.h

--- SpriteLab.h
+++ SpriteLab.h
@@ -3,15 +3,18 @@
 #define SPRITELAB_H
 
 #include <iostream>
-#include <SDL.h>
 #include <SDL_render.h>
 #include <SDL_image.h>
 #include <stdio.h>
-#include "imgui.h"
+//#include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer.h"
 #include <ImGuiFileDialog/ImGuiFileDialog.h>
 #include <imgui_internal.h>
+//#include "Layer.h"
+#include "Pixel.h"
+//#include "Project.h"
+#include "Utilities.h"
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
@@ -23,12 +26,20 @@ namespace SpriteLab
     void InitSDL();
     void Cleanup();
     void ProcessEvents();
+    void ShortcutKeys();
 
     void RenderMenuBar();
+    void RenderProjectTabs();
+    void RenderToolBar();
+    void RenderTopToolBar();
+    void RenderLayersMenu();
     void RenderCanvas();
     void RenderBackground();
+    void RenderColourPicker(ImVec2 pos);
+    void RenderProjectsMenu();
+    void RenderCreateProjectMenu();
 
-    float GetBestCanvasZoom();
+    //float GetBestCanvasZoom();
 }
 int main(int argc, char* argv[]);
 
